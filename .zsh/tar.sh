a33fac74285d1453540c80cd2d3d3c5ab9e5c15c
Nadrieril <nadrieril@gmail.com>
2015-05-24 14:08:38 +0200

    Remove deprecated tar script

--- .zsh/tar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p /tmp/zsh/.zsh
-cp -r . /tmp/zsh/.zsh
-ln -s .zsh/zshrc /tmp/zsh/.zshrc
-rm /tmp/zsh/.zsh/history
-rm -r /tmp/zsh/per-dir-history
-
-tar -czf zsh.tar.gz -C /tmp/zsh .zsh .zshrc
-rm -rf /tmp/zsh
