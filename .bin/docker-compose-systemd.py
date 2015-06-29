ddf5a7ddfdbdca48b4269e6c8a39631f87e58c3d
Nadrieril <nadrieril@gmail.com>
2015-06-29 16:30:00 +0200

    Add script to control docker-compose-generated systemd units

---
+++ .bin/docker-compose-systemd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import os
+import sys
+from subprocess import call
+
+project_name = os.path.basename(os.path.abspath('.'))
+if len(sys.argv) == 3:
+    service_name = sys.argv[2]
+    unit = "%s-%s.docker-compose.service" % (project_name, service_name)
+else:
+    unit = "%s.docker-compose.target" % project_name
+
+action = sys.argv[1]
+
+action_map = {
+    "start": "systemctl start %s",
+    "status": "systemctl status %s",
+    "stop": "systemctl stop %s",
+    "kill": "systemctl kill %s",
+    "logs": "journalctl -fu %s"
+}
+cmd = action_map[action] % unit
+call(cmd, shell=True)
